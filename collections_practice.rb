cfea3eaddc591dd0b3b63aff1d970254bfe754c9
Kirsten Keels <keelsk@gmail.com>
2020-02-24 23:14:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,8 +2,8 @@ def sort_array_asc(array_integers)
   array_integers.sort
 end
 
-def sort_array_dec(array_integers)
+def sort_array_desc(array_integers)
   array_integers.sort do |a,b|
-    b < a
+    b <=> a
   end
 end
